a56e2937a406a22c6e16769e74a0314949d78182
leviutima <levi.utima@gmail.com>
2025-05-06 21:25:24 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        Carro meuCarro1 = new Carro();
+        Carro meuCarro2 = new Carro();
+    }
+}
+
+class Carro {
+    public Carro() {
+        System.err.println("Carro criado");
+    }
+}
